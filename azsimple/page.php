2fa3386ac4e4acc1f13150dfcea54ec3910a270f
leblogdudeveloppeur <contact@leblogdudeveloppeur.fr>
2012-04-16 00:17:53 +0200

    Fix of a short bug with date in static pages

--- azsimple/page.php
+++ azsimple/page.php
@@ -5,7 +5,7 @@
 			<div class="full-post" id="post-<?php the_ID(); ?>">
 				<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php printf(__('Permanent Link to %s', 'azsimple'), get_the_title()); ?>"><?php the_title(); ?></a></h2>
 				<div class="meta">
-					<?php $timeFormat = __('M d Y', 'azsimple'); ?><?php printf(__('Published on: <span>%1$s</span> by <span>%2$s</span>', 'azsimple'), get_the_time($formatTime), get_the_author()); ?>
+					<?php $timeFormat = __('M d Y', 'azsimple'); ?><?php printf(__('Published on: <span>%1$s</span> by <span>%2$s</span>', 'azsimple'), get_the_time($timeFormat), get_the_author()); ?>
 				</div><!--meta-->
 				<div class="full-post-content"><?php the_content(); ?></div>
 				<div class="full-post-pages"><?php wp_link_pages(); ?></div>
